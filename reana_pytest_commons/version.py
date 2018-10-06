c75c63af4961155ba25149539707a083bce19575
Tibor Simko <tibor.simko@cern.ch>
2018-10-06 23:32:40 +0200

    global: license header
    * Amends license header in all files to the standardised version.
    
    * Fixes license Trove classifier in ``setup.py``.
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- reana_pytest_commons/version.py
+++ reana_pytest_commons/version.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of REANA-Pytest-Commons.
+# This file is part of REANA.
 # Copyright (C) 2018 CERN.
 #
-# REANA-Pytest-Commons is free software; you can redistribute it and/or modify
-# it under the terms of the MIT License; see LICENSE file for more details.
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 """Version information for REANA-Pytest-Commons.
 
